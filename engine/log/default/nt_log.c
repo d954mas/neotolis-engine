8e87b82045059278c19bcf36759b66370f480977
d954mas <d954mas@gmail.com>
2026-03-09 22:39:17 +0500

    fix

--- engine/log/default/nt_log.c
+++ engine/log/default/nt_log.c
@@ -1,14 +1,8 @@
 #include "log/nt_log.h"
 #include <stdio.h>
 
-void nt_log_init(void) {
-    printf("[nt_log] initialized\n");
-}
+void nt_log_init(void) { printf("[nt_log] initialized\n"); }
 
-void nt_log_info(const char *msg) {
-    printf("[INFO] %s\n", msg);
-}
+void nt_log_info(const char *msg) { printf("[INFO] %s\n", msg); }
 
-void nt_log_error(const char *msg) {
-    fprintf(stderr, "[ERROR] %s\n", msg);
-}
+void nt_log_error(const char *msg) { (void)fprintf(stderr, "[ERROR] %s\n", msg); }
--- engine/log/stub/nt_log_stub.c
+++ engine/log/stub/nt_log_stub.c
@@ -1,5 +1,5 @@
 #include "log/nt_log.h"
 
-void nt_log_init(void) { }
+void nt_log_init(void) {}
 void nt_log_info(const char *msg) { (void)msg; }
 void nt_log_error(const char *msg) { (void)msg; }
