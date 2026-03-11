ab806bd5fa2add66b295d6408f995bc27125abe4
d954mas <d954mas@gmail.com>
2026-03-11 23:02:14 +0500

    use in example

---
+++ engine/platform/web/nt_platform_web.h
@@ -0,0 +1,8 @@
+#ifndef NT_PLATFORM_WEB_H
+#define NT_PLATFORM_WEB_H
+
+/* Web-specific shell interactions. Only available in WASM builds. */
+
+void nt_platform_web_loading_complete(void);
+
+#endif /* NT_PLATFORM_WEB_H */
